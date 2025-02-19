aa1c11a09b492c7cf883928a5f583fae259784f1
kelly6bf <kelly6bf@gmail.com>
2025-02-19 14:24:06 +0900

    Subsets 풀이 개선

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/Subsets/Solution.java
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/Subsets/Solution.java
@@ -3,28 +3,17 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        answer.add(List.of());
         List<Integer> subSet = new ArrayList<>();
-        recursive(nums, answer, subSet);
-
+        recursive(nums, 0, answer, subSet);
         return answer;
     }
 
-    private void recursive(int[] nums, List<List<Integer>> answer, List<Integer> subSet) {
-        if (subSet.size() == nums.length) {
-            return;
-        }
+    private void recursive(int[] nums, int start, List<List<Integer>> answer, List<Integer> subSet) {
+        answer.add(new ArrayList<>(subSet));
 
-        for (int i = 0; i < nums.length; i++) {
-            if (subSet.contains(nums[i]) || (!subSet.isEmpty() && subSet.get(subSet.size() - 1) >= nums[i])) {
-                continue;
-            }
-
+        for (int i = start; i < nums.length; i++) {
             subSet.add(nums[i]);
-            answer.add(new ArrayList<>(subSet));
-
-            recursive(nums, answer, subSet);
-            
+            recursive(nums, i + 1, answer, subSet);
             subSet.remove(subSet.size() - 1);
         }
     }
