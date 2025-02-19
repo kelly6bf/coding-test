f637e87aebe5e5dbb7e7ce3a992b873a3757dc5d
kelly6bf <kelly6bf@gmail.com>
2025-02-19 11:02:12 +0900

    Combinations 풀이

---
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/Combinations/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> combination = new ArrayList<>();
+        recursive(n, k, 1, answer, combination);
+
+        return answer;
+    }
+
+    private void recursive(int n, int k, int start, List<List<Integer>> answer, List<Integer> combination) {
+        if (combination.size() == k) {
+            answer.add(new ArrayList<>(combination));
+        }
+
+        for (int i = start; i <= n; i++) {
+            combination.add(i);
+            recursive(n, k, i + 1, answer, combination);
+            combination.remove(combination.size() - 1);
+        }
+    }
+}
