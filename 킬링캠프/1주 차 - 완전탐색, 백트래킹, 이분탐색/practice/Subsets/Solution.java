9f381f3cf1e91137e43a6404f2d2b04f00e24a2b
kelly6bf <kelly6bf@gmail.com>
2025-05-13 13:44:23 +0900

    Subsets 풀이 코드 변경

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Subsets/Solution.java
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Subsets/Solution.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> subset = new ArrayList<>();
         List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> subSet = new ArrayList<>();
-        recursive(nums, 0, answer, subSet);
+        answer.add(new ArrayList<>());
+        backTracking(nums, 0, subset, answer);
+
         return answer;
     }
 
-    private void recursive(int[] nums, int start, List<List<Integer>> answer, List<Integer> subSet) {
-        answer.add(new ArrayList<>(subSet));
-
+    private void backTracking(int[] nums, int start, List<Integer> subset, List<List<Integer>> answer) {
         for (int i = start; i < nums.length; i++) {
-            subSet.add(nums[i]);
-            recursive(nums, i + 1, answer, subSet);
-            subSet.remove(subSet.size() - 1);
+            subset.add(nums[i]);
+            answer.add(new ArrayList<>(subset));
+            backTracking(nums, i + 1, subset, answer);
+            subset.remove(subset.size() - 1);
         }
     }
 }
