b0b29e05481376c6c6abc251848b4a00e00303aa
kelly6bf <kelly6bf@gmail.com>
2025-05-13 13:52:56 +0900

    Binary Search 풀이 코드 변경

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Binary Search/a. 반복문 풀이/Solution.java
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Binary Search/a. 반복문 풀이/Solution.java
@@ -1,9 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
+        return binarySearch(nums, target);
+    }
+
+    private int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+
         while (left <= right) {
-            int mid = (right - left) / 2 + left;
+            int mid = left + ((right - left) / 2);
             if (nums[mid] == target) {
                 return mid;
             }
