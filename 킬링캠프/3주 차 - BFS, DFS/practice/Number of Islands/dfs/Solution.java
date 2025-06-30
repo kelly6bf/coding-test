e5f03306573335ef0b3b657db11a8b1a28d3a43a
kelly6bf <kelly6bf@gmail.com>
2025-06-30 19:53:43 +0900

    Number of Islands 풀이 업로드

---
+++ 킬링캠프/3주 차 - BFS, DFS/practice/Number of Islands/dfs/Solution.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    private static final int[][] DIR = new int[][]{{-1, 0}, {0, 1}, {0, -1}, {1, 0}}; // 북, 동, 서, 남
+    private static final char LAND = '1';
+    private static final char WATER = '0';
+
+    public int numIslands(char[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int result = 0;
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                if (grid[row][col] == WATER || visited[row][col]) {
+                    continue;
+                }
+
+                dfs(grid, visited, new Node(row, col));
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(char[][] grid, boolean[][] visited, Node cur) {
+        for (int i = 0; i < 4; i++) {
+            Node next = new Node(cur.row + DIR[i][0], cur.col + DIR[i][1]);
+            if (!isSafe(grid, next) || visited[next.row][next.col]) {
+                continue;
+            }
+
+            visited[next.row][next.col] = true;
+            dfs(grid, visited, next);
+        }
+    }
+
+    private boolean isSafe(char[][] grid, Node node) {
+        int row = node.row;
+        int col = node.col;
+
+        return (row >= 0 && row < grid.length && col >= 0 && col < grid[row].length && grid[row][col] == LAND);
+    }
+}
+
+class Node {
+    int row;
+    int col;
+
+    Node(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
