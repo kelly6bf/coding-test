7743a8708f03a9e37153f8601511e91cc4f1be56
kelly6bf <kelly6bf@gmail.com>
2025-08-21 11:35:19 +0900

    Binary Search 파이썬 재귀 풀이 업로드

---
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Binary Search/b. 재귀 풀이/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.recursive(nums, target, 0, len(nums) - 1)
+    
+    def recursive(self, nums, target, left, right):
+        if (left > right):
+            return -1
+        
+        mid = left + ((right - left) // 2)
+        if (nums[mid] == target):
+            return mid
+        
+        if (nums[mid] < target):
+            return self.recursive(nums, target, mid + 1, right)
+        else:
+            return self.recursive(nums, target, left, mid - 1)
