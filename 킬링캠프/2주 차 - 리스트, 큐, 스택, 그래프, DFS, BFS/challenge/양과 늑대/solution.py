3e8b812f1fe9e1a28531bf4925be681a935f2ab8
kelly6bf <kelly6bf@gmail.com>
2025-10-22 13:20:21 +0900

    양과 늑대 파이썬 풀이 수정

--- 킬링캠프/2주 차 - 리스트, 큐, 스택, 그래프, DFS, BFS/challenge/양과 늑대/solution.py
+++ 킬링캠프/2주 차 - 리스트, 큐, 스택, 그래프, DFS, BFS/challenge/양과 늑대/solution.py
@@ -1,25 +1,24 @@
 def solution(info, edges):
-    def dfs(sheep, wolf):
-        if sheep > wolf:
-            answer.append(sheep)
-        else:
-            return
-
-        for p, c in edges:
-            if visited[p] and not visited[c]:
-                visited[c] = True
-                if info[c] == 0:
-                    dfs(sheep + 1, wolf)
-                else:
-                    dfs(sheep, wolf + 1)
-                    
-                visited[c] = False
-
-    # 메인 로직
-    visited = [False for _ in info]
     answer = []
-    
+    visited = [False for _ in info]
+
     visited[0] = True
-    dfs(1, 0)
+    dfs(1, 0, edges, info, visited, answer)
 
     return max(answer)
+
+def dfs(sheep, wolf, edges, info, visited, answer):
+    if sheep <= wolf:
+        return
+    
+    answer.append(sheep)
+
+    for p, c in edges:
+        if visited[p] and not visited[c]:
+            visited[c] = True
+            if info[c] == 0:
+                dfs(sheep + 1, wolf, edges, info, visited, answer)
+            else:
+                dfs(sheep, wolf + 1, edges, info, visited, answer)
+            
+            visited[c] = False
