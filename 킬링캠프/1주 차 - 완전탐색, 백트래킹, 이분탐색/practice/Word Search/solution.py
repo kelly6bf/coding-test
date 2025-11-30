86ce1f29b0aa7e47c0ca45f73aff0f163849f42c
kelly6bf <kelly6bf@gmail.com>
2025-11-30 18:05:03 +0900

    Word Search 새로운 풀이 업로드

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Word Search/solution.py
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Word Search/solution.py
@@ -1,41 +1,42 @@
 class Solution:
-    DIR = [[-1, 0], [0, 1], [0, -1], [1, 0]] # 북, 동, 서, 남
-
     def exist(self, board: List[List[str]], word: str) -> bool:
-        visited = [[False for _ in row] for row in board]
+        # 상수
+        DIR = [[-1, 0], [0, 1], [0, -1], [1, 0]]  # 상, 우, 좌, 하
+        M = len(board)
+        N = len(board[0])
 
-        for row in range(0, len(board)):
-            for col in range(0, len(board[row])):
-                if (board[row][col] != word[0]):
+        # 메서드
+        def search(row, col, visited, step):
+            if step == len(word):
+                return True
+            
+            for dx, dy in DIR:
+                nextRow = row + dx
+                nextCol = col + dy
+
+                if not isSafe(nextRow, nextCol) or visited[nextRow][nextCol] or board[nextRow][nextCol] != word[step]:
+                    continue
+
+                visited[nextRow][nextCol] = True
+                if search(nextRow, nextCol, visited, step + 1):
+                    return True
+                
+                visited[nextRow][nextCol] = False
+            
+            return False
+        
+        def isSafe(row, col):
+            return row >= 0 and row < M and col >= 0 and col < N
+
+        # 메인 로직
+        for row in range(M):
+            for col in range(N):
+                if board[row][col] != word[0]:
                     continue
                 
+                visited = [[False for _ in range(N)] for _ in range(M)]
                 visited[row][col] = True
-                if (self.backTracking(board, word, visited, row, col, 1)):
+                if search(row, col, visited, 1):
                     return True
-
-                visited[row][col] = False
         
         return False
-
-    
-    def backTracking(self, board, word, visited, row, col, step):
-        if (step == len(word)):
-            return True
-
-        for dr, dc in self.DIR:
-            next_row = row + dr
-            next_col = col + dc
-
-            if (not self.is_safe(board, visited, next_row, next_col) or board[next_row][next_col] != word[step]):
-                continue
-            
-            visited[next_row][next_col] = True
-            if (self.backTracking(board, word, visited, next_row, next_col, step + 1)):
-                return True
-
-            visited[next_row][next_col] = False
-        
-        return False
-    
-    def is_safe(self, board, visited, row, col):
-        return row >= 0 and row < len(board) and col >= 0 and col < len(board[row]) and not visited[row][col]
