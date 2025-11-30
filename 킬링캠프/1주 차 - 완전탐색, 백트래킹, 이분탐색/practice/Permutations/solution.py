546d3b619b2f587d2c2c8078c7039b21b854b3b2
kelly6bf <kelly6bf@gmail.com>
2025-11-30 16:31:44 +0900

    Permutations 메서드명 변경

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Permutations/solution.py
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Permutations/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         # 메서드
-        def recursive(temp, result):
+        def backTracking(temp, result):
             if len(temp) == len(nums):
                 result.append(temp[:])
                 return
@@ -11,11 +11,11 @@ class Solution:
                     continue
 
                 temp.append(i)
-                recursive(temp, result)
+                backTracking(temp, result)
                 temp.pop()
         
         # 메인 로직
         result = []
-        recursive([], result)
+        backTracking([], result)
 
         return result
