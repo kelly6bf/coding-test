e19d0492a90c8e2634ad210102ccde6f2e8fdb7b
kelly6bf <kelly6bf@gmail.com>
2025-11-30 17:16:51 +0900

    Subsets 백트래킹 새 풀이 업로드

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Subsets/solution.py
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Subsets/solution.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        self.recursive(nums, ans, [], 0)
+        # 메서드
+        def backTracking(result, temp, start):
+            result.append(temp[:])
 
-        return ans
-    
-    def recursive(self, nums, ans, temp, start):
-        if (temp not in ans):
-            ans.append(temp[:])
+            for i in range(start, len(nums)):
+                temp.append(nums[i])
+                backTracking(result, temp, i + 1)
+                temp.pop()
 
-        for i in range(start, len(nums)):
-            temp.append(nums[i])
-            self.recursive(nums, ans, temp, i + 1)
-            temp.pop()
+        # 메인 로직
+        result = []
+        backTracking(result, [], 0)
+
+        return result
