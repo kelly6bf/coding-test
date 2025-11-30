d443908073f7b71b212a931dce6a161c1235cfee
kelly6bf <kelly6bf@gmail.com>
2025-11-30 18:30:49 +0900

    Binary Search 새로운 재귀 풀이 업로드

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Binary Search/b. 재귀 풀이/solution.py
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/practice/Binary Search/b. 재귀 풀이/solution.py
@@ -1,16 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return self.recursive(nums, target, 0, len(nums) - 1)
-    
-    def recursive(self, nums, target, left, right):
-        if (left > right):
-            return -1
-        
-        mid = (left + right) // 2
-        if (nums[mid] == target):
-            return mid
-        
-        if (nums[mid] < target):
-            return self.recursive(nums, target, mid + 1, right)
-        else:
-            return self.recursive(nums, target, left, mid - 1)
+        # 메서드
+        def binarySearch(left, right):
+            if left > right:
+                return -1
+            
+            mid = left + ((right - left) // 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+            return binarySearch(left, right)
+            
+        # 메인 로직
+        return binarySearch(0, len(nums) - 1)
