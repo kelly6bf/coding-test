e2864bf72043aa9448f165a98f4ee5ef596f0e49
kelly6bf <kelly6bf@gmail.com>
2025-12-11 09:23:53 +0900

    피로도 파이썬 풀이 업로드

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/challenge/피로도/solution.py
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/challenge/피로도/solution.py
@@ -1,17 +1,23 @@
 def solution(k, dungeons):
-    visited = [False for _ in dungeons]
-    return recursive(k, dungeons, 0, visited)
+    # 전역 값
+    answer = 0
+    
+    # 메서드
+    def search(k, count, visited):
+        nonlocal answer
+        answer = max(answer, count)
 
-def recursive(k, dungeons, count, visited):
-    max_count = count
+        for i in range(len(dungeons)):
+            needHp, useHp = dungeons[i]
+            if visited[i] or k < needHp:
+                continue
+
+            visited[i] = True
+            search(k - useHp, count + 1, visited)
+            visited[i] = False
     
-    for i in range(0, len(dungeons)):
-        minimumPoint, usePoint = dungeons[i]
-        if (visited[i] or k < minimumPoint):
-            continue
-        
-        visited[i] = True
-        max_count = max(max_count, recursive(k - usePoint, dungeons, count + 1, visited))
-        visited[i] = False
-    
-    return max_count
+    # 메인 로직
+    visited = [False for _ in dungeons]
+    search(k, 0, visited)
+
+    return answer
