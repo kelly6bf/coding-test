742240b790e6e6f9fcac4cc2259cd13edc611a4e
kelly6bf <kelly6bf@gmail.com>
2026-01-01 19:45:10 +0900

    자물쇠와 열쇠 파이썬 풀이 업로드

--- 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/challenge/자물쇠와 열쇠/solution.py
+++ 킬링캠프/1주 차 - 완전탐색, 백트래킹, 이분탐색/challenge/자물쇠와 열쇠/solution.py
@@ -2,49 +2,43 @@ def solution(key, lock):
     # 전역 데이터
     M = len(key)
     N = len(lock)
-
+    
     # 메서드
     def match(keyRowOffset, keyColOffset, rot):
-        targetKey = calculateCurrentKey(rot)
-
+        curKey = getCurKey(rot)
         for lockRow in range(N):
             keyRow = lockRow + keyRowOffset
-            
             for lockCol in range(N):
                 keyCol = lockCol + keyColOffset
-
-                if not isSafe(keyRow, keyCol):
+                
+                if not keyIsSafe(keyRow, keyCol):
                     if lock[lockRow][lockCol] == 0:
                         return False
-                    else:
-                        continue
-
-                if (lock[lockRow][lockCol] == 1 and targetKey[keyRow][keyCol] == 1) or (lock[lockRow][lockCol] == 0 and targetKey[keyRow][keyCol] != 1):
+                    continue
+                
+                if (lock[lockRow][lockCol] == 1 and curKey[keyRow][keyCol] == 1) or (lock[lockRow][lockCol] == 0 and curKey[keyRow][keyCol] != 1):
                     return False
-        
         return True
-
-    def calculateCurrentKey(rot):
-        if (rot == 0):
+    
+    def getCurKey(rot):
+        if rot == 0:
             return key
-
+        
         curKey = [[0 for _ in range(M)] for _ in range(M)]
         for row in range(M):
             for col in range(M):
-                if (key[row][col] == 0):
-                    continue
-                if (rot == 1):
-                    curKey[col][M - row - 1] = 1
-                elif (rot == 2):
-                    curKey[M - row - 1][M - col - 1] = 1
+                if rot == 1:
+                    curKey[col][M - row - 1] = key[row][col]
+                elif rot == 2:
+                    curKey[M - row - 1][M - col - 1] = key[row][col]
                 else:
-                    curKey[M - col - 1][row] = 1
-        
+                    curKey[M - col - 1][row] = key[row][col]
+                
         return curKey
     
-    def isSafe(keyRow, keyCol):
-        return keyRow >= 0 and keyRow < M and keyCol >= 0 and keyCol < M
-
+    def keyIsSafe(row, col):
+        return row >= 0 and row < M and col >= 0 and col < M
+    
     # 메인 로직
     for keyRowOffset in range(M - 1, -N, -1):
         for keyColOffset in range(M - 1, -N, -1):
